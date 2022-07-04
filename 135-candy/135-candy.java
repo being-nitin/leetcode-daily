a4b759c281200a07e56c75f2e35eff1055614bd3
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-07-04 17:54:46 +0530

    Time: 2 ms (100.00%), Space: 42.9 MB (89.77%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int candy(int[] ratings) {
+     int candies = 0,n = ratings.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i] = right[i+1]+1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            candies = candies + Math.max(left[i],right[i]);
+        }
+        return candies;
+        }
+}
