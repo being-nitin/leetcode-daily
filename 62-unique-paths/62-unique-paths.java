e0bd5de1139ccb19af55bd8d9a9a633c6e75070a
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-08-01 07:47:50 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (17.36%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,10 +2,10 @@ class Solution {
     public int uniquePaths(int m, int n) {
             return comb(m+n-2, n-1);
     }
-       private int comb(int n, int r){
+       private int comb(int x, int y){
         long ans = 1;
-        for (int i=r+1; i <= n; i++){
-            ans = (ans * i)/(i-r);
+        for (int i=y+1; i <= x; i++){
+            ans = (ans * i)/(i-y);
         }
         return (int)ans;
     }
