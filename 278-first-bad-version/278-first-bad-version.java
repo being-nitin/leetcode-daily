474a1cef84edebc2164c1151da83e4a370fdc999
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-08-18 15:32:45 +0530

    Time: 27 ms (34.49%), Space: 40.9 MB (42.67%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,7 +3,7 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low=0,high = n-1;
+        int low=1,high = n;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(isBadVersion(mid)){
