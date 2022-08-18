18459155a6c98064124598b9a8590b8c7f6840e0
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-08-18 15:47:47 +0530

    Time: 2 ms (41.31%), Space: 57.5 MB (35.04%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,11 +2,9 @@ class Solution {
     public void reverseString(char[] s) {
         int low=0,high=s.length-1;
         while(low<high){
-            char temp = s[low];
-            s[low] = s[high];
-            s[high] = temp;
-            low++;
-            high--;
+            char temp = s[high];
+            s[high--] = s[low];
+            s[low++] = temp;
         }
         }
 }
