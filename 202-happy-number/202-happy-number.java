ec61b657608629f68b1c72afd27a9f3704e85069
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-09-12 17:01:33 +0530

    Time: 5 ms (12.10%), Space: 40.6 MB (83.82%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(n);
+        while(n!=1){
+            int curr = n;
+            int sum = 0;
+            while(curr!=0){
+                sum+=(curr%10)*(curr%10);
+                curr/=10;
+            }
+            if(set.contains(sum))
+                return false;
+            set.add(sum);
+            n = sum;
+            }
+        return true; 
+    }
+}
