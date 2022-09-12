7dc71f813507533b9e5500ef8cd8707ad75316ef
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-09-12 15:27:44 +0530

    Time: 2 ms (99.46%), Space: 42.2 MB (94.82%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,57 +10,26 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        // create empty node to return as head at the end of the program
-        ListNode head = new ListNode(0);
-		// create the node that will be generated
-        ListNode l3 = head;
-		// create a copy for the final step
-        ListNode last = head;
-        while(l1 != null || l2 != null) {
-            int sum = 0;
-			// if one pointer is shorter than another, then ignore the shorter one
-            if (l1 == null) {
-                sum += 0;
-            }
-            else {
-                sum += l1.val;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carry=0;
+        while(l1!=null || l2!=null){
+            int m = (l1!=null)?l1.val:0;
+            int n = (l2!=null)?l2.val:0;
+            int sum = carry+m+n;
+            carry = sum/10;
+            tail.next = new ListNode(sum%10);
+            tail = tail.next;
+            if(l1!=null){
                 l1 = l1.next;
             }
-            if (l2 == null) {
-                sum += 0;
-            }
-            else {
-                sum += l2.val;
+            if(l2!=null){
                 l2 = l2.next;
             }
-			// create the next node and shift to it
-            l3.next = new ListNode(sum);
-            l3 = l3.next;
         }
-		// if any value in the list is greater than ten, then...
-        while (last != null) {
-            if (last.val >= 10) {
-                int carry = 0;
-                if (last.next != null) {
-				// add the remainder to the next node, then shift, and so on...
-                    carry = last.val / 10;
-                    last.val = last.val % 10;
-                    last = last.next;
-                    last.val += carry;
-                }
-				// end of list case
-                else {
-                    carry = last.val / 10;
-                    last.next = new ListNode(carry); 
-                    last.val = last.val % 10;
-                    last = last.next;
-                }
-            }
-            else {
-                last = last.next;
-            }
+        if(carry>0){
+            tail.next = new ListNode(carry);
         }
-        return head.next;
+        return dummy.next;
     }
 }
