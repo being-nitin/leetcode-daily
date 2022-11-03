a975aed258116b83981b2a49c50b03703a770ce4
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2022-11-03 23:54:19 +0530

    Added solution - LeetHub

---
+++ Array Removals - GFG/array-removals.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().removals(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int removals(int[] arr, int n, int k) {
+        Arrays.sort(arr);
+        int i=0,j=0,maxSize=0;
+        while(j<n)
+        {
+            if(arr[j]-arr[i]<=k)
+                j++;
+            else if(i<j)
+                i++;
+            maxSize=Math.max(maxSize,j-i);
+        }
+        return n-maxSize;
+    }
+}
