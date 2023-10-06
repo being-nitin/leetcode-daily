f8b732f18302a52f85e20b31e6a98508a98298c1
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2023-10-06 22:41:20 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (60.18%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int cnt = 0;
+        int min = Math.min(a,b);
+        for(int i=1;i<=min;i++){
+            if(a%i==0 && b%i==0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
