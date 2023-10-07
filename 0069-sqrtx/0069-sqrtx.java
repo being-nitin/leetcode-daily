b2f59e8046f2255c65b91dd2868a412bddbb8a69
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2023-10-07 22:59:08 +0530

    Time: 1 ms (97.44%), Space: 39.3 MB (89.55%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+    int start=1;
+     int end =x;
+  
+      int result= 0;
+     while(start<=end)  {
+       int mid = start+(end-start)/2;
+     if(mid*mid==x){
+         result = mid;
+         return result;
+     }
+     else if((long) mid * mid > (long) x)
+                end = mid-1;
+         
+            else
+            {
+                result = mid;
+                start = mid + 1;
+     }
+     }
+       return result;
+    }
+}
