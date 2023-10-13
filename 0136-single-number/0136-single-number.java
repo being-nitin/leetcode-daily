b6e7950de0c7a0e7afaa722a6b605422cd1e89b3
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2023-10-13 21:18:09 +0530

    Time: 1 ms (99.93%), Space: 44.4 MB (62.14%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor^nums[i];
+        }
+        return xor;
+    
+    }
+}
