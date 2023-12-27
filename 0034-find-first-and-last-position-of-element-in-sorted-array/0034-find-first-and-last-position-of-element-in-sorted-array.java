df13b802670e269425cffb64feebc842e1a704e6
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2023-12-27 22:11:19 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (5.60%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = binarySearch1(nums,target);
-        res[1] = binarySearch2(nums,target);
-        return res;
+        int[] ans = new int[2];
+        return new int[]{first(nums,target),second(nums,target)};
         
+        
+        
+       
     }
-    public static int binarySearch1(int[] arr,int target){
-        int start = 0;
-        int end = arr.length-1;
+    private static int first(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
         int ans = -1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
+        while(low<=high){
+            int mid = low + (high-low)/2;
             if(arr[mid]==target){
                 ans = mid;
-                end = mid-1;
+                high = mid-1;
             }
             else if(target>arr[mid]){
-                start= mid+1;
+                low = mid+1;
             }
-            else if(target<arr[mid]){
-                end = mid-1;
+            else{
+                high = mid-1;
+                
             }
         }
         return ans;
     }
-    public static int binarySearch2(int[] arr,int target){
-        int start = 0;
-        int end = arr.length-1;
+     private static int second(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
         int ans = -1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
+        while(low<=high){
+            int mid = low + (high-low)/2;
             if(arr[mid]==target){
                 ans = mid;
-                start = mid+1;
+                low = mid+1;
             }
             else if(target>arr[mid]){
-                start= mid+1;
+                low = mid+1;
             }
-            else if(target<arr[mid]){
-                end = mid-1;
+            else{
+                high = mid-1;
+                
             }
         }
         return ans;
     }
+    
 }
