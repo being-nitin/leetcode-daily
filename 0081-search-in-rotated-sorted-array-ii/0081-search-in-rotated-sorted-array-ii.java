ce4a0ef697831d604aa76d4888ac93189bf02b15
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2024-02-07 21:29:49 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (82.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-    int low = 0;
+//      Modified Binary Search
+        int low = 0;
         int high = nums.length-1;
         while(low<=high){
-            int mid = low + (high-low)/2;
+            int mid = low +(high-low)/2;
             if(nums[mid]==target){
                 return true;
             }
@@ -14,24 +15,22 @@ class Solution {
                 high--;
             }
             if(nums[low]<=nums[mid]){
-                if((target>=nums[low] && target<nums[mid])){
+                if(target>=nums[low] && target<nums[mid]){
                     high = mid-1;
                 }
-                   else{
-                       low = mid+1;
-                   }
-                   }
-                   else{
-                       if(target>nums[mid] && target<=nums[high]){
-                           low = mid+1;
-                       }
-                       else{
-                           high = mid-1;
-                       }
-                       
-                   }
-    }
-                   return false;
-    
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(target>nums[mid] && target<=nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
     }
 }
