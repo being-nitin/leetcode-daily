4d5ed6d69f3f156753ced013b5a5f9095eabbf55
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2024-02-13 21:43:34 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (27.11%) - LeetHub

--- 1175-prime-arrangements/1175-prime-arrangements.java
+++ 1175-prime-arrangements/1175-prime-arrangements.java
@@ -1,33 +1,36 @@
 class Solution {
-    private static final int mod = 1_000_000_007;
-public int numPrimeArrangements(int n) {
-    int totalPrime = 0;
-    for(int i=1;i<=n;i++){
-        if(prime(i)){
-            totalPrime = totalPrime+1;
+    private static final int mod = 1000000007;
+    public int numPrimeArrangements(int n) {
+        int totalPrime = 0;
+        for(int i=1;i<=n;i++){
+            if(isPrime(i)){
+                totalPrime = totalPrime+1;
+            }
         }
-    }
-    int totalNonPrime = n-totalPrime;
-    long fact = (factorial(totalPrime)*factorial(totalNonPrime))%mod;
-    return (int)fact;
+        int nonPrime = n- totalPrime;
+        long fact = (factorial(totalPrime)*factorial(nonPrime))%mod;
+        return (int)fact;
         
     }
+    // to calculate factorial 
+    
     private long factorial(int n){
-        long result = 1;
+        long res = 1;
         for(int i=2;i<=n;i++){
-            result = (result*i)%mod;
+            res = (res*i)%mod;
         }
-        return result;
+        return res;
     }
-        
-    private boolean prime(int n){
+    
+    // to calculate that it is a prime number or not
+    private boolean isPrime(int n){
         if(n<=1){
             return false;
         }
         for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                return false;
-            }
+        if(n%i==0){
+            return false;
+        }
         }
         return true;
     }
