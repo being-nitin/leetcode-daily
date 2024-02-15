7bc643d52dc200a518cc1f159c06b9192cc984fd
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2024-02-15 22:14:27 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (29.04%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n!=0){
+            n = n/5;
+            count = count+n;
+        }
+        return count;
+    }
+}
