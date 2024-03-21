f3f2e1ff22d55850dbc95824995e7cd8e91f6e08
Nitin Singh <82897280+being-nitin@users.noreply.github.com>
2024-03-21 21:34:12 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (34.75%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.java
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public int alternateDigitSum(int n) {
-       int count = 0;
-        int a = n;
+    int count = 0;
+    int a = n;
         while(a!=0){
             a = a/10;
             count++;
         }
-        int temp = 1, sum=0;
+        int temp =1,sum = 0;
         if(count%2!=0){
             while(n!=0){
-                sum = sum+ temp*(n%10);
+                sum = sum + temp*(n%10);
                 temp = temp*-1;
                 n = n/10;
             }
         }
         else{
             while(n!=0){
-                sum = sum+(temp*(-1))*(n%10);
+                sum = sum +(temp*(-1))*(n%10);
                 temp = temp*-1;
-                n =n/10;
+                n = n/10;
             }
         }
         return sum;
-        
     }
 }
